37ff8265c465719666332eb547eeb52136d2fea9
Anas OUFDOU <oufdou.anas@gmail.com>
2019-11-12 20:58:50 +0100

    checkstyle

--- src/main/java/org/anasoid/tomio/valves/TomioValve.java
+++ src/main/java/org/anasoid/tomio/valves/TomioValve.java
@@ -1,17 +1,15 @@
 /*
  * Copyright 2002-2019 the original author or authors.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package org.anasoid.tomio.valves;
@@ -23,14 +21,17 @@ import org.apache.catalina.connector.Response;
 import org.apache.catalina.valves.ValveBase;
 
 /**
- * 
+ *  TomioValve : main valve for Tomio.
  * @author anasoid
  * @since 1.0
  */
-public class TomioValve  extends ValveBase{
+public class TomioValve extends ValveBase {
 
-  /* (non-Javadoc)
-   * @see org.apache.catalina.Valve#invoke(org.apache.catalina.connector.Request, org.apache.catalina.connector.Response)
+  /*
+   * (non-Javadoc)
+   * 
+   * @see org.apache.catalina.Valve#invoke(org.apache.catalina.connector.Request,
+   * org.apache.catalina.connector.Response)
    */
   @Override
   public void invoke(Request request, Response response) throws IOException, ServletException {
